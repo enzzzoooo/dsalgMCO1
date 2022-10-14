13c6980229d26012f0d195ef4597eb828c5eaec6
lyannna <63532129+lyannna@users.noreply.github.com>
2022-10-14 23:38:50 +0800

    MergeSort with string randomizer

---
+++ Suffix.java
@@ -0,0 +1,24 @@
+public class Suffix {
+
+    private String stringDNA;
+    private int index;
+
+    public Suffix(String stringDNA, int index) {
+        this.stringDNA = stringDNA;
+        this.index = index;
+    }
+
+    public String getStringDNA() {
+        return stringDNA;
+    }
+    public void setStringDNA(String stringDNA) {
+        this.stringDNA = stringDNA;
+    }
+    public int getIndex() {
+        return index;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+}
